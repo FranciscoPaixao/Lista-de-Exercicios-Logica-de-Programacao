8dd6125bd23799b002d5edd55ae28d66b02ffd69
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-03-09 14:13:29 -0300

    .

---
+++ Lista-de-Exercicios-Logica-de-Programacao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Exercicios_Logica_de_Programacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
